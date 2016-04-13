f10f20a8256e65f2b532b426887f2e03ec498d74
MattLamont <matthewlamont454@gmail.com>
2016-04-13 17:22:33 -0600

    added dummy commands

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ var math = require('mathjs');
 
 module.exports = {
     checkForCommand: function( image_path ){
-        return math.random( 1 , 4 );
+        var random_num = math.random( 1 , 4 );
+        var floor_num = math.floor( random_num );
+
+        if( floor_num == 1 ){
+            return String("command1");
+        }
+
+        else if( floor_num == 2 ){
+            return String("command2");
+        }
+
+        else if( floor_num == 3 ){
+            return String("command2");
+        }
+
+        else if( floor_num == 4 ){
+            return String("command4");
+        }
+
+        else{
+            return String("command1");
+        }
     }
 };
