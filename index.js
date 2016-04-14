e16c04e899f286dd542362ea02c10b2bc5375fd3
MattLamont <matthewlamont454@gmail.com>
2016-04-14 13:02:00 -0600

    spawns child process

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var math = require('mathjs');
 
 module.exports = {
-    checkForCommand: function( image_path ){
+    checkForCommand: function( image_path , callback ){
+        /*
         var random_num = math.randomInt( 1 , 5 );
 
         if( random_num == 1 ){
@@ -24,5 +25,38 @@ module.exports = {
         else{
             return String("no_command");
         }
+        */
+
+        var exec = require( 'child_process' ).exec;
+
+        var process_string = './ShapeDetect ';
+        process_string += image_path;
+        
+        exec( process_string , function exec_callback( error , stdout , stderr ){
+
+            if( error ){
+                return error;
+            }
+
+            if( stderr.indexOf( 'no_command' ) > -1 ){
+                return 'no_command';
+            }
+
+            else if( stderr.indexOf( 'command1' ) > -1 ){
+                return 'command1';
+            }
+
+            else if( stderr.indexOf( 'command2' ) > -1 ){
+                return 'command2';
+            }
+
+            else if( stderr.indexOf( 'command3' ) > -1 ){
+                return 'command3';
+            }
+
+            else{
+                return 'no_command';
+            }
+        });
     }
 };
