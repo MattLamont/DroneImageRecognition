bcb00c7cd642824e33ee578575dc43ef572f7850
MattLamont <matthewlamont454@gmail.com>
2016-04-14 13:20:02 -0600

    added error to callbacks

--- index.js
+++ index.js
@@ -1,61 +1,38 @@
 
-var math = require('mathjs');
 
 module.exports = {
-    checkForCommand: function( image_path , callback ){
-        /*
-        var random_num = math.randomInt( 1 , 5 );
-
-        if( random_num == 1 ){
-            return String("no_command");
-        }
-
-        else if( random_num == 2 ){
-            return String("command1");
-        }
-
-        else if( random_num == 3 ){
-            return String("command2");
-        }
-
-        else if( random_num == 4 ){
-            return String("command3");
-        }
-
-        else{
-            return String("no_command");
-        }
-        */
+    checkForCommand: function( program_name , image_path , callback ){
 
         var exec = require( 'child_process' ).exec;
 
-        var process_string = './ShapeDetect ';
+        var process_string = program_name;
+        process_string += " ";
         process_string += image_path;
 
         exec( process_string , function exec_callback( error , stdout , stderr ){
 
             if( error ){
-                callback( error );
+                callback( error , "no_command" );
             }
 
             if( stderr.indexOf( 'no_command' ) > -1 ){
-                callback( 'no_command' );
+                callback( error , 'no_command' );
             }
 
             else if( stderr.indexOf( 'command1' ) > -1 ){
-                callback( 'command1' );
+                callback( error , 'command1' );
             }
 
             else if( stderr.indexOf( 'command2' ) > -1 ){
-                callback( 'command2' );
+                callback( error , 'command2' );
             }
 
             else if( stderr.indexOf( 'command3' ) > -1 ){
-                callback( 'command3' );
+                callback( error , 'command3' );
             }
 
             else{
-                callback( 'no_command' );
+                callback( error , 'no_command' );
             }
         });
     }
